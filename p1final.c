4d63afa432a02a0665fee17a5f4aafcef4059e01
Spoorthi M S <97089639+SpoorthiMS0207@users.noreply.github.com>
2022-03-09 03:56:32 +0000

    addition of fractions

--- p1final.c
+++ p1final.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("Enter the numnerator and denominator of fractions\n");
+  scanf("%d%d%d%d",num1,den1,num2,den2);
+}
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+  *num3=(num1*den2)+(den1*num2);
+  *den3= den1*den2;
+  
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+  printf("sum of %d/%d and %d/%d is %d/%d\n",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int num1,den1,num2,den2,num3,den3;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&num3,&den3);
+  output(num1,den1,num2,den2,num3,den3);
+  return 0;
+}
+
+}
+
